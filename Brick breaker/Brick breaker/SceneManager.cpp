9c5260d9543cbb7896704c474d940a2753f97558
bielskialeksi <92392918+bielskialeksi@users.noreply.github.com>
2024-12-17 14:52:57 +0100

    Update SceneManager.cpp

--- Brick breaker/Brick breaker/SceneManager.cpp
+++ Brick breaker/Brick breaker/SceneManager.cpp
@@ -11,10 +11,14 @@ SceneManager::~SceneManager()
 
 void SceneManager::CreateNextScene()
 {
+	sm_Order.push(Scene());
+	sm_Order.back().CreateBricks();
 }
 
 void SceneManager::ChangeScene()
 {
+	sm_Order.pop();
+	sm_Order.front().LoadScene();
 }
 
 Ball* SceneManager::GetBall()
