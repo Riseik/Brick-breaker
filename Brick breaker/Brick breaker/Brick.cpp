1e896dcf4abfd67c5b91870a67b0117210db24f7
joachim-gabin <ejoachimgabin@gaming.tech>
2024-12-18 08:35:59 +0100

    change image path
    change image path

--- Brick breaker/Brick breaker/Brick.cpp
+++ Brick breaker/Brick breaker/Brick.cpp
@@ -6,7 +6,7 @@ Brick::Brick()
 
 Brick::Brick(float x, float y)
 {
-	m_TexturePath = "metal_brick.jpg";
+	m_TexturePath = "ressource/metal_brick.png";
 	m_pos = { x,y };
 	m_scale = { 80,40 };
 	LoadTexture(m_TexturePath);
